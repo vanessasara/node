d414840be10d4f177fa63e4dc21594f8d1d950f0
vanessasara <vanessasarah911@gmail.com>
2024-07-20 04:50:47 +0500

    Initial commit

---
+++ project01_number_guessing-game/main.ts
@@ -0,0 +1,23 @@
+import("inquirer").then(async (inquirer) => {
+    const { prompt } = inquirer.default; 
+
+    type ans = {
+        userguess: number
+    };
+
+    const systemgeneratedno = Math.floor(Math.random() * 10);
+    const answer: ans = await prompt([{
+        type: "number",
+        name: "userguess",
+        message: "Enter your guess b/w 1 to 10"
+    }]);
+    const { userguess } = answer;
+    console.log(`Your guess: ${userguess}, System's number: ${systemgeneratedno}`);
+    if (userguess === systemgeneratedno) {
+        console.log("Yay! Your answer is correct. You win!");
+    } else {
+        console.log("Try again, better luck next time!");
+    }
+}).catch((error) => {
+    console.error("Error loading inquirer:", error);
+});
